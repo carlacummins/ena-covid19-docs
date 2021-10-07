b1e8fe0640685188ca355271214e1f3f95fdb6b9
Carla Cummins <carlac@ebi.ac.uk>
2021-10-07 16:28:04 +0100

    Add project info to config

--- conf.py
+++ conf.py
@@ -1,1 +1,6 @@
 extensions = ['myst_parser']
+
+# General information about the project.
+project = u'ENA SARS-CoV-2 Documentation'
+copyright = u'2021, European Nucleotide Archive (ENA). Licensed under the Apache License 2.0.'
+author = u'The European Nucleotide Archive (ENA) team.'
